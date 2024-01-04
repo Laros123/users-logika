52e7cbc646b0aa79498d4d05f54846d4a9167eb5
Laros123 <you@example.com>
2024-01-04 20:27:17 +0200

    add functional get and create user

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+import crud
+import schemas
 
 
 app = FastAPI()
 
+users = []
 
 
+@app.get('/users/{user_id}')
+def get_user(user_id: int):
+    return crud.get_user_by_id(users, user_id)
+
+
+@app.get('/users')
+def get_users():
+    return crud.get_all_users(users)
+
+
+@app.post('/create_user')
+def create_user(user: schemas.User):
+    crud.create_user(users, user)
--- schemas.py
+++ schemas.py
@@ -1,2 +1,6 @@
 from pydantic import BaseModel
 
+
+class User(BaseModel):
+    username: str
+    email: str
